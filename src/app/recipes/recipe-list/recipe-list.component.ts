e1d2b17f025a6b5accb6f8f37168832a32188a05
kozlovs <kozostas@gmail.com>
2017-06-06 17:44:25 +0300

    From work

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,27 +8,27 @@ import {style, state, trigger, transition, animate} from '@angular/animations';
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
-  animations: [
-    trigger('animatedInOut', [
-      state('in', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      transition('void => *', [
-        style({
-          opacity: 0,
-          transform: 'translateX(-100px)'
-        }),
-        animate(300)
-      ]),
-      transition('* => void', [
-        animate(300, style({
-          transform: 'translateX(-100px)',
-          opacity: 0,
-        }))
-      ]),
-    ])
-  ]
+  // animations: [
+  //   trigger('animatedInOut', [
+  //     state('in', style({
+  //       opacity: 1,
+  //       transform: 'translateX(0)'
+  //     })),
+  //     transition('void => *', [
+  //       style({
+  //         opacity: 0,
+  //         transform: 'translateX(-300px)'
+  //       }),
+  //       animate(300)
+  //     ]),
+  //     transition('* => void', [
+  //       animate(300, style({
+  //         transform: 'translateX(-300px)',
+  //         opacity: 0,
+  //       }))
+  //     ]),
+  //   ])
+  // ]
 })
 export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
